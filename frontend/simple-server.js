7ccb2d8af6049ea9f6210d0e55311e38c191a292
Zoro330 <hellzone330@gmail.com>
2025-04-01 23:40:24 +0800

    Fix CSP issues with comprehensive approach: postbuild script, permissive headers

--- frontend/simple-server.js
+++ frontend/simple-server.js
@@ -2,10 +2,31 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files with no CSP restrictions
+// Add permissive security headers to all responses
+app.use((req, res, next) => {
+  // Set very permissive CSP headers to allow all resources
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src * 'unsafe-inline' 'unsafe-eval'; font-src * data:; img-src * data:; connect-src *"
+  );
+  
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  next();
+});
+
+// Serve static files from the build directory
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Respond to all GET requests by serving index.html
+// Handle OPTIONS requests
+app.options('*', (req, res) => {
+  res.status(200).send();
+});
+
+// For any request that doesn't match the static files, send index.html
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
